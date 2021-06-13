d8b258bbc4076d405c117ed8fd4f92eac5187a82
ZhouTian0716 <egglord0716@gmail.com>
2021-06-13 15:14:23 +1000

    save version

--- script.js
+++ script.js
@@ -2,39 +2,35 @@
 const signUp = () => {
     alert("Sign Up Successful");
     //direct to signup route
+
 }
 
 const login = () => {
     alert("Login Successful");
     //direct to login route
+
 }
 
 
+
+// Modal Switching feature
 const homepageModalToSign = () => {
+    $("#emailInput").val("");
+    $("#passwordInput").val("");
     // Change Modal attributes
     $("#homeModal").find("form").attr("id", "form-signUp");
     $("#homeModal").find("h5").text("Sign Up");
-    $("#homeModal").find(".password").children(".form-control").attr("id", "passwordSignUp");
-    // Any eventlisener targeting the elements inside a dynamic modal should be inside the modal updating function.
+    // element.on() method should be after the creation of that element
     $("#form-signUp").on("submit", signUp);
-    // console.log($("#passwordSignUp").attr("type"));
-
-    if( $("#passwordSignUp").attr("type") === "password" ) { $("#toggolePassword").on("click", passwordShow ) }
-        else { $("#toggolePassword").on("click", passwordhide ) }
-    
 };
-
 const homepageModalToLogin = () => {
+    $("#emailInput").val("");
+    $("#passwordInput").val("");
     // Change Modal attributes
     $("#homeModal").find("form").attr("id", "form-login");
     $("#homeModal").find("h5").text("Login");
-    // Any eventlisener targeting the elements inside a dynamic modal should be inside the modal updating function.
+    // element.on() method should be after the creation of that element
     $("#form-login").on("submit", login);
-    $("#form-login").on("click", "#toggolePassword", () => {
-        console.log($("#passwordInput").attr("type"));
-        if( $("#passwordInput").attr("type") === "password" ) { passwordShow() }
-        else { passwordhide() }
-    })
 };
   
 $("#signUpBtn").on("click", homepageModalToSign);
@@ -44,17 +40,15 @@ $("#loginBtn").on("click", homepageModalToLogin);
 const passwordShow = () => {
     $("#homeModal").find(".password").children(".form-control").attr("type", "text");
     // $("#passwordInput").attr("type", "text");
-    $("#toggolePassword").attr("class", "fas fa-eye-slash");
+    $("#toggolePw").attr("class", "fas fa-eye-slash");
 }
 const passwordhide = () => {
     $("#homeModal").find(".password").children(".form-control").attr("type", "password");
     // $("#passwordInput").attr("type", "password");
-    $("#toggolePassword").attr("class", "fas fa-eye");
+    $("#toggolePw").attr("class", "fas fa-eye");
 }
 
-// const toggoleLisener = () => {
-//     $("#toggolePassword").on("click", () => {
-//         if( $("#passwordInput").attr("type") === "password" ) { passwordShow() }
-//         else { passwordhide() }
-//     })
-// }
+$(document).on("click", "#toggolePw", () => {
+    if ( $("#passwordInput").attr("type")==="password" ){ passwordShow() }
+    else { passwordhide() }
+});
